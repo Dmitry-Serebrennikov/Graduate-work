f8364b40aecb405a584467716f72729f55d0b89b
Lovushka Dokera <54707589+Dmitry-Serebrennikov@users.noreply.github.com>
2022-03-09 08:20:46 +0800

    add northern hemisphere image in polar coordinates

--- simurg_data_visualization_beginning.py
+++ simurg_data_visualization_beginning.py
@@ -1,5 +1,7 @@
 #TODO: формат файла hdf5 - прочитать с помощью библиотеки h5py
 
+import os
+
 #библиотеки для работы с данными
 import h5py
 import numpy as np
@@ -15,7 +17,7 @@ lats = np.array([])
 lons = np.array([])
 ampltd_tec_vals = np.array([])
 
-with h5py.File(test_file, 'r') as hdf_file:
+with h5py.File(os.path.dirname(__file__) + '\\' + test_file, 'r') as hdf_file:
     hdf_base_items = list(hdf_file.items()) #структура файла hdf5 с наименованиями групп и кол-вом элементом в них
     #print(hdf_base_items)
     data = hdf_file.get('data')
@@ -42,17 +44,28 @@ with h5py.File(test_file, 'r') as hdf_file:
     ###### данные имеют вид: широта, долгота, амплитудное значение TEC
 
 #TODO: отобразить данные на карте с помощью библиотек matplotlib и cartopy
-fig = plt.figure(figsize=(10, 5))
-ax = plt.axes(projection=ccrs.PlateCarree())
 
-plt.title("2017-05-28 03:30:00.000000", fontsize=18, fontweight='bold')
-gl = ax.gridlines(draw_labels=True, dms=True, color='gray', alpha=0.5, linestyle='--')
-gl.top_labels = False
-gl.right_labels = False
+# fig = plt.figure(figsize=(10, 5))
+# ax = plt.axes(projection=ccrs.PlateCarree())
+#
+# plt.title("2017-05-28 03:30:00.000000", fontsize=18, fontweight='bold')
+# gl = ax.gridlines(draw_labels=True, dms=True, color='gray', alpha=0.5, linestyle='--')
+# gl.top_labels = False
+# gl.right_labels = False
+#
+# ax.coastlines()
+# plt.scatter(lons, lats, c=ampltd_tec_vals, cmap='jet', marker=',', lw=0.5, s=2, transform=ccrs.PlateCarree())
+# plt.colorbar(label='TECu/min', pad=0.05)
+# plt.show()
+#
+# fig.savefig('2017-05-28_03-30-00-000000_TEC.png')
 
+#TODO: добавить блок с изображением северного полушария и в полярных координатах
+fig = plt.figure(figsize=(5, 5))
+ax = plt.axes(projection=ccrs.Orthographic(0, 90))
 ax.coastlines()
-plt.scatter(lons, lats, c=ampltd_tec_vals, cmap='jet', marker=',', lw=0.5, s=2, transform=ccrs.PlateCarree())
-plt.colorbar(label='TECu/min', pad=0.05)
+ax.gridlines(draw_labels=True, dms=True, color='gray', alpha=0.5, linestyle='--')
+#plt.scatter(lons, lats, c=ampltd_tec_vals, cmap='jet', marker=',', lw=0.5, s=2, transform=ccrs.PlateCarree())
 plt.show()
 
-fig.savefig('2017-05-28_03-30-00_000000.png')
+fig.savefig('polar_coords.png')
